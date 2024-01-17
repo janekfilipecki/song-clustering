1a8bbbc52ff787b4ec457f3de76e566e98a76b50
janekfilipecki <filipeckijan@gmail.com>
2024-01-17 18:48:35 +0100

    added playlist preparation scripts'

--- models/k-means/train.py
+++ models/k-means/train.py
@@ -1,25 +1,34 @@
 import wandb
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
+import pandas as pd
 
 wandb.login()
 
 
-wandb.init(project="ium-projekt", entity="ium-team", group="k-means")
+wandb.init(project="ium-projekt", entity="ium-team",
+           group="k-means", name="num cluster expermient")
 
-wandb.log({"dataset": wandb.Table(data="TODO")})
 
-k = 3  # Number of clusters
+data = df = pd.read_json(
+    "../../data_v3/data/preprocessed_tracks.jsonl", lines=True)
 
-wandb.config.k = k
+numerical_columns = ['popularity', 'duration_ms',
+                     'danceability', 'energy', 'key', 'loudness',
+                     'speechiness', 'acousticness', 'instrumentalness',
+                     'liveness', 'valence', 'tempo',
+                     'popularity_from_sessions', 'release_date_numeric']
 
-kmeans = KMeans(n_clusters=k)
-kmeans.fit("DATA")
-labels = kmeans.labels_
+data_numerical = data[numerical_columns]
 
-wandb.log({"cluster_assignments": wandb.Histogram(labels)})
-inertia = kmeans.inertia_
-wandb.log({"inertia": inertia})
-wandb.log({"silhouette_score": silhouette_score("DATA", labels)})
+for k in range(2, 50, 3):
+    kmeans = KMeans(n_clusters=k, n_init=10)
+    kmeans.fit(data_numerical)
+    labels = kmeans.labels_
+
+    inertia = kmeans.inertia_
+    wandb.log({"num_clusters": k})
+    wandb.log({"inertia": inertia})
+    wandb.log({"silhouette_score": silhouette_score(data_numerical, labels)})
 
 wandb.finish()
